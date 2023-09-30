17fc06668a90e0f8a4a1794f42c6da391cd276d1
JohnCorby <williamforgames6@gmail.com>
2023-09-30 13:42:32 -0700

    init commit

--- ArkoseSurprise/ArkoseSurprise.cs
+++ ArkoseSurprise/ArkoseSurprise.cs
@@ -11,19 +11,17 @@ namespace ArkoseSurprise;
 [HarmonyPatch]
 public class ArkoseSurprise : ModBehaviour
 {
+	private static ArkoseSurprise Instance;
+
 	private void Awake()
 	{
+		Instance = this;
 		Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 	}
 
 	private void Start()
 	{
-		// Starting here, you'll have access to OWML's mod helper.
-		ModHelper.Console.WriteLine($"My mod {nameof(ArkoseSurprise)} is loaded!", MessageType.Success);
-
-
-		// Example of accessing game code.
-		LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+		LoadManager.OnCompleteSceneLoad += (_, loadScene) =>
 		{
 			if (loadScene != OWScene.SolarSystem) return;
 			ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
@@ -33,11 +31,15 @@ public class ArkoseSurprise : ModBehaviour
 	}
 
 
+	// https://codingvision.net/c-make-a-critical-process-bsod-if-killed
 	[DllImport("ntdll.dll", SetLastError = true)]
 	private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
 
 	private static void Crash()
 	{
+		Instance.ModHelper.Console.WriteLine("crash");
+		return;
+
 		int isCritical = 1; // we want this to be a Critical Process
 		int BreakOnTermination = 0x1D; // value for BreakOnTermination (flag)
 
@@ -45,5 +47,12 @@ public class ArkoseSurprise : ModBehaviour
 
 		// setting the BreakOnTermination = 1 for the current process
 		NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, ref isCritical, sizeof(int));
+	}
+
+
+	[HarmonyPostfix, HarmonyPatch(typeof(KidRockController), nameof(KidRockController.Start))]
+	private static void KidRockController_Start(KidRockController __instance)
+	{
+		Instance.ModHelper.Console.WriteLine("arkose hook");
 	}
 }
